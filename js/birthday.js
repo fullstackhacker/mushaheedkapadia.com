79a443a9cdeb87266240e94859a01b7f7615f912
Mushaheed Kapadia <kapadiamush@gmail.com>
2016-05-02 10:08:47 -0400

    remove birthday counter

--- js/birthday.js
+++
@@ -1,5 +0,0 @@
-setInterval(function(){
-    var birthdayString = "02/19/" + new Date().getFullYear() + " 10:01:00 AM";
-    var birthday = moment(birthdayString, "MM/DD/YYYY HH:mm:ss A", true);
-    $('#birthday-counter').html(birthday.fromNow() + ".");
-}, 1000);
